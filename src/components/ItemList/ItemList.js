4dbc16991d7f72738fe94019e05cf05d1f6e8128
Mauro Mena <mauromenae@gmail.com>
2022-05-26 02:34:40 -0300

    Desafio Map y promises

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,29 +1,107 @@
 import '../ItemList/itemList.css'
 import Item from '../Item/Item';
 import Grid from '@mui/material/Grid';
-import ItemCount from '../ItemCount/ItemCount';
+import { useState, useEffect } from 'react';
 
 
 const ItemList = () => {
+    const [products,setProducts] = useState ([])
+    const productos = [
+    {
+        nombre : 'Hamburgesa con queso',
+        precio : 500,
+        stock : 4,
+        descripcion: '',
+        id : 1,
+        imagen : 'burger1.jpg'
+
+
+
+    },
+    {
+        nombre : 'Burger completa',
+        precio : 700,
+        stock : 5,
+        descripcion: '',
+        id : 2,
+        imagen : 'burger1.jpg'
+
+
+    },
+    {
+        nombre : 'Combo con papas cheddar',
+        precio : 900,
+        stock : 10,
+        descripcion: '',
+        id : 3,
+        imagen : 'burger1.jpg'
+
+
+    },
+    {
+        nombre : 'Triple Bacon y cheddar con papas',
+        precio : 1200,
+        stock : 8,
+        descripcion: '',
+        id : 4,
+        imagen : 'burger1.jpg'
+
+
+    },
+    {
+        nombre : 'Super doble doble',
+        precio : 1400,
+        stock : 10,
+        descripcion: '',
+        id : 5,
+        imagen : 'burger1.jpg',
+
+
+    },
+
+    ]
     
+    const getProductos = () => {
+        return new Promise ( (resolve, reject) => {
+            setTimeout(() => {
+                resolve(productos)
+            }, 2000)
+            
+        } )
+    }
+
+    useEffect(()=> {
+
+        getProductos()
+        .then((res)=> {
+            setProducts(res)
+        })
+        .catch((err)=> {
+        })
+        .finally(() => {
+        })
+
+    },[])
+
+   
+
     return (
         <>
             <Grid container className="contenedor-lista-productos" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
-                <Grid item >
-                    <Item producto="Burger con queso" precio="500" stock="4">xs=2</Item>
-                </Grid>
-                <Grid item>
-                    <Item producto="Burger completa" precio="700" stock="5">xs=2</Item>
-                </Grid>
-                <Grid item>
-                    <Item producto="Combo con papas" precio="900" stock="9">xs=2</Item>
-                </Grid>
-                <Grid item>
-                    <Item producto="Triple sarpada" precio="1200" stock="3">xs=2</Item>
-                </Grid>
-                <Grid item>
-                    <Item producto="Super doble doble" precio="1320" stock="11">xs=2</Item>
-                </Grid>
+                {
+                products.map(({nombre, precio, stock,id}) => {
+                    
+                    return (
+                        
+                            <div>
+                                <Grid item key={id}>
+                                    <Item producto={nombre} precio={precio} stock={stock}>xs=2</Item>
+                                </Grid>
+                            </div>
+                        
+                    )
+                })
+                }
             </Grid>
         </>
     )
